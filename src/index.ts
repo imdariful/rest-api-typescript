9dcd34de459f75084d10e0e11b8764ee9cd1f845
Md.Ariful Islam <hi@imdariful.com>
2023-09-19 09:26:34 +0600

    route created

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -29,4 +30,6 @@ const MONGO_URL =
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error) );
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
